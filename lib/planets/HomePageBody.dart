0e58c764ab10cbe21034aefbc241f543dbef01c8
Jones Wang <jones.wang@gsit.com.tw>
2018-05-29 18:33:43 +0800

    add card content

--- lib/planets/HomePageBody.dart
+++ lib/planets/HomePageBody.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/planets/models/Planet.dart';
+import 'package:flutter_demo/planets/TextStyle.dart';
 
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return new PlanetRow();
+    return new PlanetRow(planets[0]);
   }
 }
 
 class PlanetRow extends StatelessWidget {
+
+  static Planet _planet = new Planet();
+  PlanetRow(Planet planet){
+    _planet = planet;
+  }
+
   final planetThumbnail = new Container(
     margin: new EdgeInsets.symmetric(
       vertical: 16.0
     ),
     alignment: FractionalOffset.centerLeft,
     child: new Image(
-        image: new AssetImage('assets/images/mars.png'),
+        image: new AssetImage(_planet.image),
       height: 92.0,
       width: 92.0,
     ),
@@ -37,6 +45,30 @@ class PlanetRow extends StatelessWidget {
       ]
     ),
   );
+
+  final planetCardContent = new Container(
+    margin: new EdgeInsets.fromLTRB(76.0, 16.0, 16.0, 16.0),
+    constraints: new BoxConstraints.expand(),
+    child: new Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        new Container(height: 4.0,),
+        new Text(_planet.name,
+        style: Style.headerTextStyle,),
+        new Container(height: 10.0,),
+        new Text(_planet.location,
+        style: Style.subHeaderTextStyle,),
+        new Container(
+          margin: new EdgeInsets.symmetric(vertical: 8.0),
+          height: 2.0,
+          width: 18.0,
+          color: new Color(0xff00c6ff),
+        )
+      ],
+    ),
+  );
+  
+
   @override
   Widget build(BuildContext context) {
     return new Container(
